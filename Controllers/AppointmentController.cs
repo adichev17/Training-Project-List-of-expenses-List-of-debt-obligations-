38e07bff672a687914e3d234575365285f49ea60
Dmitry <adichev17@gmail.com>
2021-08-22 13:41:09 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Training_Project__List_of_expenses_and_List_of_debt_obligations_.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Training_Project__List_of_expenses_and_List_of_debt_obligations_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
